441a24a333855597b0626ac82dfd4a6eda9ccf45
Brad Westfall <brad@azpixels.com>
2021-05-13 11:59:53 -0700

    renaming lesson

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log(`Welcome to the ReactTraining.com Node Workshop`)
-console.log(`Node Version: ${process.version}\n`)
+console.log(`You Node Version: ${process.version}\n`)
+console.log(
+  "Doing `npm start` on this repo doesn't do much. Take a peek at the package.json scripts. \n"
+)
