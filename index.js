dd1ddec92d8bb1f98e56058d2c9e62e70e3a24cd
Brad Westfall <brad@azpixels.com>
2023-03-06 14:49:54 -0500

    wording

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-console.log(`Welcome to the ReactTraining.com Node Workshop`)
-console.log(`You Node Version: ${process.version}\n`)
+console.log(`Welcome to the ReactTraining.com Node & JS Workshop`)
+console.log(`Your Node Version: ${process.version}\n`)
 console.log(
   "Doing `npm start` on this repo doesn't do much. Take a peek at the package.json scripts. \n"
 )
