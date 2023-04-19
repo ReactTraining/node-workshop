d3554fddec56fd84f3fd6863babcc8984b20dc6a
Brad Westfall <brad@builtwithweb.com>
2023-04-19 10:50:30 -0400

    small changes

--- index.js
+++
@@ -1,5 +0,0 @@
-console.log(`Welcome to the ReactTraining.com Node & JS Workshop`)
-console.log(`Your Node Version: ${process.version}\n`)
-console.log(
-  "Doing `npm start` on this repo doesn't do much. Take a peek at the package.json scripts. \n"
-)
